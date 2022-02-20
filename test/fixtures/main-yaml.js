210d9c45df19a797ff80d037ef651879d3f57994
Matthias Zaunseder <zaunseder@outlook.com>
2022-02-20 14:45:48 +0100

    Add unit tests

--- test/fixtures/main-yaml.js
+++ test/fixtures/main-yaml.js
@@ -1,3 +1,8 @@
 import api from './api.yaml';
 
 t.is(api.paths['/my/path'].get.summary, 'Some GET request');
+t.is(
+  api.paths['/my/path'].get.responses['200'].content['application/json'].schema
+    .type,
+  'object'
+);
--- test/test.js
+++ test/test.js
@@ -1,19 +1,41 @@
 const test = require('ava');
 const rollup = require('rollup');
+const typescript = require('@rollup/plugin-typescript');
 
 const openapi = require('..');
 
-test('converts yaml', async (t) => {
+async function setup(fileName, additionalPlugins = []) {
   const build = await rollup.rollup({
-    input: './fixtures/main-yaml.js',
+    input: 'test/fixtures/' + fileName,
     output: {
       file: 'bundle.js',
-      format: 'iife'
+      format: 'iife',
     },
-    plugins: [openapi()],
+    plugins: [openapi(), ...additionalPlugins],
   });
-  const code = await build.generate(
-  )
+  const code = await build.generate({});
 
-  console.log(code.output);
+  // console.log(code.output[0].code);
+
+  return new Function('t', code.output[0].code);
+}
+
+test('converts yaml', async (t) => {
+  const fn = await setup('main-yaml.js');
+
+  fn(t);
+});
+
+test('converts yml', async (t) => {
+  const fn = await setup('main-yml.js');
+
+  fn(t);
+});
+
+test('converts yaml in TypeScript', async (t) => {
+  const fn = await setup('main-yaml.ts', [
+    typescript({ lib: ['es5', 'es6', 'dom'], target: 'es5' }),
+  ]);
+
+  fn(t);
 });
